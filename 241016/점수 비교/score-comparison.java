3cfecc47f0d6df15d967ac4ef820950965453e95
HyungLuck <noko625@gmail.com>
2024-10-16 21:47:41 +0900

    Title: 점수 비교, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 241016/점수 비교/score-comparison.java
+++ 241016/점수 비교/score-comparison.java
@@ -2,17 +2,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int amath, bmath, aEng, bEng;
+        int a = sc.nextInt();
+       int a2 = sc.nextInt();
 
-        amath = sc.nextInt();
-        bmath = sc.nextInt();
+       int b = sc.nextInt();
+       int b2 = sc.nextInt();
 
-        aEng = sc.nextInt();
-        bEng = sc.nextInt();
 
-        if(amath >= bmath && aEng >= bEng )
-            System.out.println("1");
-        else
-            System.out.println("0");
+       System.out.print( a>b && a2>b2 ? "1" : "0" );
     }
 }
