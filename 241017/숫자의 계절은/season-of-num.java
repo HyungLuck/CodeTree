3b0bf2e3dec4bcd3f1ec8e26a77649b2479a8efa
HyungLuck <noko625@gmail.com>
2024-10-17 17:16:50 +0900

    Title: 숫자의 계절은, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 241017/숫자의 계절은/season-of-num.java
+++ 241017/숫자의 계절은/season-of-num.java
@@ -9,7 +9,7 @@ public class Main {
         System.out.print("Winter");
         
     else if(m == 3||m == 4 ||m == 5)
-        System.out.print("Sprint");
+        System.out.print("Spring");
 
     else if(m == 6||m == 7 ||m == 8)
         System.out.print("Summer");
