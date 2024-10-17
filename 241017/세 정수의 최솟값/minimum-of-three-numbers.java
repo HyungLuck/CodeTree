86cf054c4e3316d6200d5809d227abc5d28c383a
HyungLuck <noko625@gmail.com>
2024-10-17 11:23:32 +0900

    Title: 세 정수의 최솟값, Time: 188ms, Memory: 10MB, Status: Passed - CodeTree

--- 241017/세 정수의 최솟값/minimum-of-three-numbers.java
+++ 241017/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -10,7 +10,7 @@ public class Main {
         System.out.print(a);
     else if(b<=a && b<=c)
         System.out.print(b);
-    else (c<=a && c<=b)
+    else
         System.out.print(c);
     }
 }
