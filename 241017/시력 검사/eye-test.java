6220c6c7198c100f5fded8b5c917489455361498
HyungLuck <noko625@gmail.com>
2024-10-17 11:07:15 +0900

    Title: 시력 검사, Time: 142ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241017/시력 검사/eye-test.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double a = sc.nextDouble();
+    double b = sc.nextDouble();
+
+    if(a>=1 && b>=1)
+        System.out.print("High");
+    else if (a>=0.5 && b>= 0.5)
+        System.out.print("Middle");
+    else  
+        System.out.print("Low");
+
+    }
+}
