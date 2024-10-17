5653e7107c4cf644b50029b29cd2e419759e37bf
HyungLuck <noko625@gmail.com>
2024-10-17 20:12:33 +0900

    Title: 일 수 구하기, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 241017/일 수 구하기/number-of-days-in-month.java
+++ 241017/일 수 구하기/number-of-days-in-month.java
@@ -4,7 +4,7 @@ public class Main {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
 
-    if(a == 1 || a==3 || a==5 || a==7 || a==8 || a==10 || a==1 )
+    if(a == 1 || a==3 || a==5 || a==7 || a==8 || a==10 || a==12 )
             System.out.print(31);
     else if(a== 4 || a==6 || a==9 || a==11)
             System.out.print(30);
