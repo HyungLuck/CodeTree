fa0995baf408cfbb292219e033f17f2d5b890b8c
HyungLuck <noko625@gmail.com>
2024-10-19 20:07:23 +0900

    Title: 100점 도달하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241019/100점 도달하기/reach-100-point.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    for(int i = n; i<=100; i++){
+        if(90<=i)
+            System.out.print("A"+ " ");
+        else if(80<=i)
+            System.out.print("B"+ " ");
+        else if(70<=i)
+            System.out.print("C"+ " ");
+        else if(60<=i)
+            System.out.print("D"+ " ");
+        else
+            System.out.print("F"+ " ");
+    }
+    }
+}
